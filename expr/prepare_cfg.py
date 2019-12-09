9aad40e4ff35db97ef78d453124e2b763e18c964
Jia Chen <jiac@t470>
2019-12-09 14:24:20 -0500

    sync

--- expr/prepare_cfg.py
+++ expr/prepare_cfg.py
@@ -6,6 +6,7 @@ sys.path.append('../')
 import model.birnn
 import model.vanilla
 import model.conv_transformer
+import model.multi_conv_transformer
 
 
 '''func
@@ -146,7 +147,55 @@ def prepare_conv_transformer():
     os.mkdir(outprefix)
 
 
+def prepare_multi_conv_transformer():
+  root_dir = '/data/jiac/meva' # diva
+  trn_data_dirs = [
+    os.path.join(root_dir, 'compile_2', 'trn'),
+  ]
+  val_data_dir = os.path.join(root_dir, 'compile_2', 'val')
+  expr_dir = os.path.join(root_dir, 'expr', 'multi_conv_transformer')
+  ft_name = 'of'
+
+  params = {
+    'num_pos_class': 35,
+    'dim_hiddens': [256],
+    'num_step': 128,
+    'kernel_size': 17,
+    'focal_loss': True,
+    'num_head': 8,
+    'dim_embed': 256,
+    'dim_ft': 1024,
+    'norms': False,
+    'shift': 64/2,
+    'num_layer': 1,
+  }
+
+  outprefix = '%s/%s.%s.%d.%d.%d.%d.%d'%(
+    expr_dir, ft_name, '_'.join([str(d) for d in params['dim_hiddens']]), 
+    params['num_head'], params['focal_loss'], 
+    params['num_step'], params['kernel_size'], params['num_layer']
+  )
+  model_cfg_file = '%s.model.json'%outprefix
+  cfg = model.multi_conv_transformer.gen_cfg(**params)
+  cfg.num_epoch = 100
+  cfg.save(model_cfg_file)
+
+  path_cfg = {
+    'trn_dirs': trn_data_dirs,
+    'val_dir': val_data_dir,
+    'output_dir': outprefix,
+    'label2lid_file': os.path.join(root_dir, 'label.json')
+  }
+  path_cfg_file = '%s.path.json'%outprefix
+  with open(path_cfg_file, 'w') as fout:
+    json.dump(path_cfg, fout, indent=2)
+
+  if not os.path.exists(outprefix):
+    os.mkdir(outprefix)
+
+
 if __name__ == '__main__':
   # prepare_birnn()
   # prepare_vanilla()
-  prepare_conv_transformer()
+  # prepare_conv_transformer()
+  prepare_multi_conv_transformer()
